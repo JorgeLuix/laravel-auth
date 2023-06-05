5b5635d610da9b6acf3c8e946bab0e3b85a188f5
JorgeLuix <tec.jorge.29@gmai.com>
2023-06-05 15:05:16 +0200

    modify navHeader, add sidebar

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Creat Post</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -24,14 +25,15 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function() {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-});
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function() {
+//     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 // });
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('posts', PostController::class);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
